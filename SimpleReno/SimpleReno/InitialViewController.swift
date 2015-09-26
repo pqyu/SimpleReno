efe88a54c14835873fdbdb21a656d458da8421f9
flCer <kelvin.kl.lau@gmail.com>
2015-09-25 17:42:47 -0700

    Initial commit

---
+++ SimpleReno/SimpleReno/InitialViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  SimpleReno
+//
+//  Created by Kelvin on 2015-09-25.
+//  Copyright © 2015 Noms. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+}
