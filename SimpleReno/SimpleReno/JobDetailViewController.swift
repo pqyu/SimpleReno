dc6a800d9b2d9c24c84aaebc8aac6c26fd7fd7cf
flCer <kelvin.kl.lau@gmail.com>
2015-09-25 21:37:49 -0700

    added some stuff, now working on JobDetailView

---
+++ SimpleReno/SimpleReno/JobDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  JobDetailViewController.swift
+//  SimpleReno
+//
+//  Created by Kelvin on 2015-09-25.
+//  Copyright © 2015 Noms. All rights reserved.
+//
+
+import UIKit
+
+class JobDetailViewController: UITableViewController {
+    
+}
+
--- SimpleReno/SimpleReno/JobsTableViewController.swift
+++ SimpleReno/SimpleReno/JobsTableViewController.swift
@@ -11,8 +11,18 @@ import ParseUI
 import Parse
 
 class JobsTableViewController: PFQueryTableViewController {
+    private struct SegueIdentifiers {
+        static let JobDetailSegue = "JobDetailSegue"
+    }
+
     override func queryForTable() -> PFQuery {
         return Job.query()!
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == SegueIdentifiers.JobDetailSegue {
+            //segue to detail, give it the job
+        }
     }
 }
 
